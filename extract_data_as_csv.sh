68ab3631d62af035c04d76f732de0198ed136106
Remy <remy.tang@telecom-paris.fr>
2021-10-23 15:24:58 +0200

    fixed handoverhand data not working

--- extract_data_as_csv.sh
+++ extract_data_as_csv.sh
@@ -4,7 +4,7 @@ dir=.
 output=${dir}/output
 bin=${bin}/bin
 java=java
-filename=_data.csv
+filename=data.csv
 threads="1 4 6 8 10 12"
 sizes="100 1000 10000"
 writes="0 10 100" # update ratios
@@ -20,18 +20,17 @@ fi
 
 benchs="linkedlists.lockbased.CoarseGrainedListBasedSet linkedlists.lockbased.HandOverHandListBasedSet linkedlists.lockbased.LazyLinkedListSortedSet"
 for bench in ${benchs}; do
-  dest=$(${output}/data/${bench:22}_${filename})
-  touch ${dest}
-  echo "throughput,threads,uratio,lsize" > ${dest}
+  touch ${output}/data/${bench:22}_${filename}
+  echo "throughput,threads,uratio,lsize" > ${output}/data/${bench:22}_${filename}
   for write in ${writes}; do
     for t in ${threads}; do
        for i in ${sizes}; do
-         grep Throughput ${dir}/${output}/cleaned_log/${bench}-i${i}-u${write}-t${t}-w${warmup}-d${duration}.log | awk -v ORS="," '{print $3}' >> ${dest}
-         echo -n ${t} >> ${dest}
-         echo -n ',' >> ${dest}
-         echo -n ${write} >> ${dest}
-         echo -n ',' >> ${dest}
-         echo ${i} >> ${dest}
+         grep Throughput ${dir}/${output}/log/${bench}-i${i}-u${write}-t${t}-w${warmup}-d${duration}.log | awk -v ORS="," '{print $3}' >> ${output}/data/${bench:22}_${filename}
+         echo -n ${t} >> ${output}/data/${bench:22}_${filename}
+         echo -n ',' >> ${output}/data/${bench:22}_${filename}
+         echo -n ${write} >> ${output}/data/${bench:22}_${filename}
+         echo -n ',' >> ${output}/data/${bench:22}_${filename}
+         echo ${i} >> ${output}/data/${bench:22}_${filename}
        done
     done
   done
